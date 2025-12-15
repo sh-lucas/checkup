e231466037db429fbb01b9f105c5bca2f1533481
Lucas Schwalm Silva <lucas.schwalm@roxcode.dev>
2025-12-15 17:55:56 -0300

    fix: pool --> poll

--- src/database.rs
+++ src/database.rs
@@ -3,14 +3,14 @@ use std::env;
 pub async fn setup_database() -> sqlx::SqlitePool {
     // database setup:
     let db_uri = env::var("DATABASE_URL").expect("DATABASE_URL must be set");
-    let pool = sqlx::SqlitePool::connect(&db_uri)
+    let poll = sqlx::SqlitePool::connect(&db_uri)
         .await
         .expect("Unable to open database");
 
     sqlx::migrate!()
-        .run(&pool)
+        .run(&poll)
         .await
         .expect("Failed to run database migrations");
 
-    pool
+    poll
 }
